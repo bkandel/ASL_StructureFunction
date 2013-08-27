04b21c411debcc447c5cf1d27d792e053c184349
bkandel <bkandel@seas.upenn.edu>
2013-08-27 12:40:39 -0400

    WIP: Simulation.
    Now, adding to the simulation a "structural" component of perfusion ('func.nii.gz') that comes from surface curvature.  The idea is to show that the patch-based decomposition can separate the structural component of the functional image from the ``purely functional'' part.

--- sim.R
+++ sim.R
@@ -4,22 +4,23 @@
 # antsImageWrite(t1, 'r16.nii.gz')
 library(ANTsR)
 set.seed(1213) # for reproducibility
-
+tic <- Sys.time()
 ### Set up parameters ####
-nloc <- 1000 # number of locations for added activity
-noise <- c(150, 1500)
+nloc <- 200 # number of locations for added activity
+nblob <- 300 # number of locations for structure-based activity
+noise <- c(150, 300)
 area <- 37 # brodmann area to take
-randsrc.amp <- 15
-blob.amp <- 15
-speckle.amp <- 15
-specklegm.amp <- 20
-smooth.sigma <- 2
+randsrc.amp <- 35
+blob.amp <- 35
+speckle.amp <- 10
+specklegm.amp <- 5
+smooth.sigma <- 1.5
 
 
-recompute <- F
+reseg <- F # recompute segmentation?
+regenerate <- F # regenerate sources and smoothing
 aslres <- F
-for(specklegm.amp in c(40)){
-if(recompute){
+if(reseg){
   t1 <- antsImageRead(getANTsRData('mni'), 3)
   lab <- antsImageRead(getANTsRData('mnib'), 3)
   blob <- new('antsImage', 'float', 3)
@@ -50,6 +51,7 @@ if(recompute){
 } 
 t1 <- antsImageRead('data/simulation/mni.nii.gz', 3)
 mymask <- getMask(t1)
+antsImageWrite(mymask, 'data/simulation/mask.nii.gz')
 csf <- antsImageRead('data/simulation/csf.nii.gz', 3)
 gm <- antsImageRead('data/simulation/gm.nii.gz', 3)
 wm <- antsImageRead('data/simulation/wm.nii.gz', 3)
@@ -58,37 +60,62 @@ gm.mask <- maskImage(seg, seg, 2)
 gm.mask[gm.mask>0] <- 1
 antsImageWrite(gm.mask, 'data/simulation/gm_mask.nii.gz')
 
-mynoise <- rep(0, length(gm.mask[gm.mask>0]))
-locs <- round(runif(nloc, min=0, max=length(mynoise)))
-mynoise[locs] <- runif(length(locs), min=noise[1], max=noise[2])
-mynoise.img <- antsImageClone(gm.mask)
-mynoise.img[mymask>0] <- 0
-mynoise.img[gm.mask>0] <- mynoise
-SmoothImage(3, mynoise.img, 1, mynoise.img)
-ImageMath(3, mynoise.img, 'm', mynoise.img, 5)
-antsImageWrite(mynoise.img, 'data/simulation/noise.nii.gz')
-system(paste("SurfaceBasedSmoothing data/simulation/noise.nii.gz 1", 
-             "data/simulation/gm_mask.nii.gz data/simulation/noise_smooth.nii.gz 40"))
-mynoise.img <- antsImageRead('data/simulation/noise_smooth.nii.gz', 3)
-myblob <- antsImageRead('data/simulation/blob.nii.gz', 3)
-system(paste("SurfaceBasedSmoothing data/simulation/blob.nii.gz 1", 
-             "data/simulation/gm_mask.nii.gz data/simulation/blob_smooth.nii.gz 10"))
-myblob <- antsImageRead('data/simulation/blob_smooth.nii.gz', 3)
+if(regenerate){
+  src.img <- new('antsImage', 'float', 3) 
+  ImageMath(3, src.img, 'BlobDetector', maskImage(t1, gm.mask), 100)
+  maskImage(src.img, gm.mask)
+  ImageMath(3, src.img, 'MD', src.img, 3)
+  antsImageWrite(src.img, 'data/simulation/sources.nii.gz')
+  system(paste("SurfaceBasedSmoothing data/simulation/sources.nii.gz 1", 
+               "data/simulation/gm_mask.nii.gz data/simulation/sources.nii.gz 5"))
+  
+  mynoise <- rep(0, length(gm.mask[gm.mask>0]))
+  locs <- round(runif(nloc, min=0, max=length(mynoise)))
+  mynoise[locs] <- 1 #runif(length(locs), min=noise[1], max=noise[2])
+  mynoise.img <- antsImageClone(gm.mask)
+  mynoise.img[mymask>0] <- 0
+  mynoise.img[gm.mask>0] <- mynoise
+  ImageMath(3, mynoise.img, 'MD', mynoise.img, 2)
+  antsImageWrite(mynoise.img, 'data/simulation/noise.nii.gz')
+  system(paste("SurfaceBasedSmoothing data/simulation/noise.nii.gz 1", 
+               "data/simulation/gm_mask.nii.gz data/simulation/noise_smooth.nii.gz 5"))
+  mynoise.img <- antsImageRead('data/simulation/noise_smooth.nii.gz', 3)
+  myblob <- antsImageRead('data/simulation/blob.nii.gz', 3)
+  system(paste("SurfaceBasedSmoothing data/simulation/blob.nii.gz 1", 
+               "data/simulation/gm_mask.nii.gz data/simulation/blob_smooth.nii.gz 5"))
+}
+system(paste("SurfaceCurvature data/simulation/mni.nii.gz data/simulation/curve.nii.gz", 
+             "1.5 0"))
+curve <- antsImageRead('data/simulation/curve.nii.gz', 3)
+cerebrum <- antsImageRead('data/simulation/cerebrum.nii.gz', 3)
+curve <- maskImage(curve, cerebrum)
+curve[cerebrum>0] <- curve[cerebrum>0] - 128
+curve[curve<0] <- 0
+ImageMath(3, curve, 'm', curve, 10)
+antsImageWrite(curve, 'data/simulation/curve.nii.gz')
 
+mydist <- antsImageRead('data/simulation/tmp.nii.gz', 3)
+mydist <- maskImage(mydist, cortex)
+myblob <- antsImageRead('data/simulation/blob_smooth.nii.gz', 3)
+mynoise.img <- antsImageRead('data/simulation/noise_smooth.nii.gz', 3)
+src.img <- antsImageRead('data/simulation/curve.nii.gz', 3)
 func <- antsImageClone(t1)
+func[func != 0] <- 0
 
 func[mymask>0] <- csf[mymask>0] * 5 +
   gm[mymask>0] * 100 +  wm[mymask>0] * 0.4 * 100 + 
-  + myblob[mymask>0] * blob.amp + mynoise.img[mymask>0] * randsrc.amp + 
-  rnorm(length(mymask[mymask>0]), sd=10) * speckle.amp
-func[gm.mask>0] <- func[gm.mask>0] + rnorm(length(gm.mask[gm.mask>0]), sd=10) * specklegm.amp
+  mynoise.img[mymask>0] * randsrc.amp * 5+ 
+  rnorm(length(mymask[mymask>0]), sd=10) * speckle.amp + 
+  src.img[mymask>0] * blob.amp 
+func[gm.mask>0] <- func[gm.mask>0] + 
+  rnorm(length(gm.mask[gm.mask>0]), sd=10) * specklegm.amp 
 SmoothImage(3, func, smooth.sigma, func)
-antsImageWrite(func, paste('data/simulation/func', specklegm.amp, '.nii.gz', sep=''))
+antsImageWrite(func, 'data/simulation/func.nii.gz')
 
 if(aslres){
   ResampleImageBySpacing(list(3, func, func, 1, 1, 1))
   antsImageWrite(func, 'data/simulation/func_upsample.nii.gz')
 }
 
-
-}
+toc <- Sys.time()
+print(difftime(toc, tic))
